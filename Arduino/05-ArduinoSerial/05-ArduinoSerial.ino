027fcfdd7609a06dd289b18e18ba87515272ba8d
Ronald van Leuwen <rvleuwen@gmail.com>
2017-11-19 18:48:52 +0100

    Adds node-red integration via serial, Firmata, and MQTT.

--- Arduino/05-ArduinoSerial/05-ArduinoSerial.ino
+++ Arduino/05-ArduinoSerial/05-ArduinoSerial.ino
@@ -1,3 +1,4 @@
+const int ledPin = 3;
 const int lightPin = A0;
 
 unsigned long previousMillis = 0;
@@ -5,15 +6,22 @@ const long interval = 500;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
+  Serial.setTimeout(100);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  if (Serial.available()) {
+    int ledValue = Serial.parseInt();
+    analogWrite(ledPin, ledValue);
+  }
+
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
     int light = analogRead(lightPin);
     Serial.println(light);
-  }
+  }  
 }
